a992272b166f2bd33562f25e627bcf8e1f69d6a3
Kilian Steenman <37879201+KilianSteenman@users.noreply.github.com>
2023-03-26 22:14:22 +0200

    Add all characters unlocked trophy to super smash

--- src/games/super_smash_bros.c
+++ src/games/super_smash_bros.c
@@ -28,14 +28,23 @@ char did_finish_break_the_targets(FILE *saveState, Character character) {
     return is_equal(saveState, 0x470 + 0x20 * character, 10);
 }
 
+char get_unlocked_character_count(FILE *saveState) {
+    char count = 0;
+    count += is_flag_set(saveState, 0x457, 0b1);
+    count += is_flag_set(saveState, 0x457, 0b10);
+    count += is_flag_set(saveState, 0x457, 0b100);
+    count += is_flag_set(saveState, 0x457, 0b1000);
+    return count;
+}
+
 char get_targets_completed_count(FILE *saveState) {
-    char completed_count = 0;
+    char count = 0;
     for (int i = MARIO; i <= NESS; i++) {
         if (did_finish_break_the_targets(saveState, i)) {
-            completed_count++;
+            count++;
         }
     }
-    return completed_count;
+    return count;
 }
 
 void get_game_data_super_smash_bros(Game *game, FILE *saveState) {
@@ -50,4 +59,6 @@ void get_game_data_super_smash_bros(Game *game, FILE *saveState) {
                     is_flag_set(saveState, 0x457, 0b1000));
     add_counter_trophy(game, "Target smasher", "Complete 'Break the Targets!' with all characters", GOLD,
                        12, get_targets_completed_count(saveState));
+    add_counter_trophy(game, "Family reunion", "Unlock all secret characters", GOLD,
+                       4, get_unlocked_character_count(saveState));
 }
