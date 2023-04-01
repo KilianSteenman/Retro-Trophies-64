7eba7df5ad0b4d8549d70957c2e3915e015d8bec
Kilian Steenman <37879201+KilianSteenman@users.noreply.github.com>
2023-04-01 14:26:26 +0200

    Read entire save data into memory instead of using fseek

--- src/games/super_mario_64.c
+++ src/games/super_mario_64.c
@@ -9,13 +9,14 @@
 #include "../save_state_utils.h"
 #include "../game.h"
 
-int get_coin_star_count(FILE *saveState) {
+int get_coin_star_count(char *save_data) {
     // There are 15 coin counts starting at offset 0x25
-    fseek(saveState, 0x25, SEEK_SET);
+//    fseek(saveState, 0x25, SEEK_SET);
     int coinStarsCollected = 0;
     char coinCount;
     for (int i = 0; i < 15; i++) {
-        fread(&coinCount, 1, sizeof(coinCount), saveState);
+        coinCount = save_data[0x25 + i];
+//        fread(&coinCount, 1, sizeof(coinCount), saveState);
         if (coinCount >= 100) {
             coinStarsCollected++;
         }
@@ -24,13 +25,25 @@ int get_coin_star_count(FILE *saveState) {
     return coinStarsCollected;
 }
 
+int raw_is_greater_or_equal(char *save_data, int address, int requiredValue) {
+//    unsigned short value = 0;
+//    fseek(saveState, address, SEEK_SET);
+//    fread(&value, 1, sizeof(value), saveState);
+    return save_data[address] >= requiredValue;
+}
+
 void get_game_data_mario64(Game *game, FILE *saveState) {
+    char save_data[512];
+    if(fread(&save_data, sizeof(save_data), 1, saveState)) {
+        printf("Read mario save game into memory\n");
+    }
+
     strcpy(game->title, "Super Mario 64");
     game->trophyCount = 0;
     add_bool_trophy(game, "Welcome to the third dimension", "Collect your first star", BRONZE,
-                    is_greater_or_equal(saveState, 0xC, 1));
+                    raw_is_greater_or_equal(save_data, 0xC, 1));
     add_bool_trophy(game, "Bob-omb boom!", "Collect all stars on Bob-omb battlefield", SILVER,
-                    is_greater_or_equal(saveState, 0xC, 0b11111110));
+                    raw_is_greater_or_equal(save_data, 0xC, 0b11111110));
     add_counter_trophy(game, "It's all about the money", "Collect all coin stars", SILVER,
-                       15, get_coin_star_count(saveState));
+                       15, get_coin_star_count(save_data));
 }
