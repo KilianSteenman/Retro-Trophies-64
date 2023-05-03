000039883387650a4af249e17e2daacd19febb65
Kilian Steenman <37879201+KilianSteenman@users.noreply.github.com>
2023-05-03 19:52:16 +0200

    Zelda OOT - Add medallion trophies

--- src/games/zelda_oot.c
+++ src/games/zelda_oot.c
@@ -16,6 +16,10 @@ int get_skulltula_count(char *save_data) {
     return read_short(save_data, 0xF0);
 }
 
+bool has_all_magic_upgrades(char *save_data) {
+    return save_data[0x52] == 2;
+}
+
 void get_trophy_data_zelda_oot(Game *game, char *save_data) {
     add_bool_trophy(game, "A wimp is still a wimp", "Get access to the Great Deku Tree", BRONZE,
                     raw_is_flag_set(save_data, 0xEF5, 0b00010000));
@@ -25,8 +29,22 @@ void get_trophy_data_zelda_oot(Game *game, char *save_data) {
                     raw_is_flag_set(save_data, 0x0C5, 0b00001000));
     add_bool_trophy(game, "Don't tell my father...", "Obtain Zora's Sapphire", SILVER,
                     raw_is_flag_set(save_data, 0x0C5, 0b00010000));
+
+    add_bool_trophy(game, "The flow of time is always cruel...", "Obtain the Forest Medallion", SILVER,
+                    raw_is_flag_set(save_data, 0x0C7, 0b00000001));
+    add_bool_trophy(game, "It is something that grows over time...", "Obtain the Fire Medallion", SILVER,
+                    raw_is_flag_set(save_data, 0x0C7, 0b00000010));
+    add_bool_trophy(game, "Time passes, people move...", "Obtain the Water Medallion", SILVER,
+                    raw_is_flag_set(save_data, 0x0C7, 0b00000100));
+    add_bool_trophy(game, "Past, present, future...", "Obtain the Spirit Medallion", SILVER,
+                    raw_is_flag_set(save_data, 0x0C7, 0b00001000));
+    add_bool_trophy(game, "The infinite darkness that absorbs even time...", "Obtain the Shadow Medallion", SILVER,
+                    raw_is_flag_set(save_data, 0x0C7, 0b00010000));
+
     add_bool_trophy(game, "Creepy little buggers", "Collect your first Gold Skulltula", BRONZE,
                     raw_is_flag_set(save_data, 0x0C5, 0b10000000));
-    add_counter_trophy(game, "At least it's not 900", "Collect all Gold Skulltulas", SILVER,
+    add_bool_trophy(game, "You're a wizard Link", "Obtain all magic upgrades", GOLD,
+                    has_all_magic_upgrades(save_data));
+    add_counter_trophy(game, "At least it's not 900", "Collect all Gold Skulltulas", GOLD,
                        100, get_skulltula_count(save_data));
 }
